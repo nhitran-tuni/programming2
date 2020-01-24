d03ad8ded34018baf8dc05ee76bffd2eb8fbcb08
Your Name <you@example.com>
2020-01-25 00:26:30 +0200

    h

---
+++ student/02/temperature/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float temperature;
+    cout << "Enter a temperature: ";
+    cin >> temperature;
+
+    cout << temperature << " degrees Celsius is " << 1.8 * temperature + 32 << " degrees Fahrenheit" << "!\n";
+    cout << temperature << " degrees Fahrenheit is " << (temperature - 32)/1.8 << " degrees Celsius" << endl;
+
+    return 0;
+}
