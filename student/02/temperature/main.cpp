0fdecdcd41552ccc7535d5fb4950a456ddce67b5
Your Name <you@example.com>
2020-01-25 00:28:49 +0200

    h

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -8,7 +8,7 @@ int main()
     cout << "Enter a temperature: ";
     cin >> temperature;
 
-    cout << temperature << " degrees Celsius is " << 1.8 * temperature + 32 << " degrees Fahrenheit" << "!\n";
+    cout << temperature << " degrees Celsius is " << 1.8 * temperature + 32 << " degrees Fahrenheit" << "\n";
     cout << temperature << " degrees Fahrenheit is " << (temperature - 32)/1.8 << " degrees Celsius" << endl;
 
     return 0;
