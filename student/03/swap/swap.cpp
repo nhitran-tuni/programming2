464e8da1c2e9507a802a223ec731b871d4279205
Your Name <you@example.com>
2020-01-27 17:57:31 +0200

    first submission

---
+++ student/03/swap/swap.cpp
@@ -0,0 +1,15 @@
+#include "swap.hh"
+#include <iostream>
+
+using namespace std;
+
+void swap (int& a,int& b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+
+// TODO: Implement swap function here
+
+
