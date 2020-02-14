38e44ee98c505934fe5d5b44a564b75acda19f3d
Your Name <you@example.com>
2020-02-15 00:00:03 +0200

    first

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::string input_filename = "";
+    std::cout << "Input file: ";
+    getline(std::cin,input_filename);
+
+    char separator = '.';
+    std::string output_filename = "";
+    std::string::size_type EndIndex;
+    EndIndex = input_filename.find(separator, 0);
+    output_filename = input_filename.substr(0, EndIndex) + ".output";
+    std::cout << "Output file: " << output_filename << std::endl;
+
+    std::ifstream file_object(input_filename);
+    if (not file_object)
+    {
+        std::cout << "Error! The file not_a_file.input cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        unsigned int number = 1;
+        std::string line;
+
+        std::ofstream outfile;
+        outfile.open(output_filename);
+
+        while ( getline(file_object, line) )
+        {
+            std::string num_str = std::to_string(number);
+            std::string new_line = num_str + line;
+            outfile << new_line << std::endl;
+            number += 1;
+        }
+    }
+
+    return 0;
+}
