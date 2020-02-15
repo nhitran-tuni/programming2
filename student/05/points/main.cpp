de77c34e870c3e77f92f39171b7ab4669a3e83e5
Your Name <you@example.com>
2020-02-15 20:32:31 +0200

    first

---
+++ student/05/points/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+int main()
+{
+    std::string filename;
+    std::cout << "Input file: ";
+    getline(std::cin, filename);
+
+    std::ifstream file_object(filename);
+
+    if (not file_object)
+    {
+        std::cout << "Error! The file " << filename << " cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        std::map <std::string, unsigned int> points;
+        std::string line;
+        char separator = ':';
+        std::string::size_type EndIndex;
+
+        while (getline(file_object,line))
+        {
+            EndIndex = line.find(separator, 0);
+            if (points.find(line.substr(0, EndIndex)) == points.end())
+            {
+                points.insert({line.substr(0, EndIndex), stoi(line)});
+            }
+            else
+            {
+                points.at(line.substr(0, EndIndex)) += stoi(line);
+            }
+        }
+        file_object.close();
+
+        std::cout << "Final scores:" << std::endl;
+        for (auto player : points)
+        {
+            std::cout << player.first << ": " << player.second << std::endl;
+        }
+    }
+    return 0;
+}
