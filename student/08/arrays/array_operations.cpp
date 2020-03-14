594d92b1285495f6decc8406dd34378f59d63a91
Your Name <you@example.com>
2020-03-14 04:11:56 +0200

    modify the reverse function

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -28,7 +28,7 @@ void copy(int* itemptr, int* endptr, int* targetptr) {
 }
 
 void reverse(int* leftptr, int* rightptr) {
-    while (leftptr < rightptr) {
+    while (leftptr <= rightptr) {
         int end = *(rightptr - 1 );
         *rightptr = *leftptr;
         *leftptr = end;
