1d99fefc11591347ac6df52fa5a26fcb212183fe
Your Name <you@example.com>
2020-04-02 19:04:15 +0300

    10.2.1 first submission

---
+++ student/10/calculator/calculations.cpp
@@ -0,0 +1,29 @@
+// Note that there is no need for "using namespace std",
+// since no C++ standard libraries are used.
+
+double addition(double left, double right) {
+    return left + right;
+}
+
+
+double subtraction(double left, double right) {
+    return left - right;
+}
+
+
+double multiplication(double left, double right) {
+    return left * right;
+}
+
+
+double division(double left, double right) {
+    return left / right;
+}
+
+double power(double left, double right) {
+    double result = 1;
+    for (double i = 0; i < right; i++) {
+        result *= left;
+    }
+    return result;
+}
