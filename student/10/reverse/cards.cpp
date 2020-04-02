c51231a30f2ccbdbb99bbf73b159998844d131b9
Your Name <you@example.com>
2020-04-02 20:06:09 +0300

    10.3.1 first submission

---
+++ student/10/reverse/cards.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <memory>
+#include "cards.hh"
+
+
+Cards::Cards(): top_( nullptr ) {
+}
+
+
+void Cards::add(int id) {
+    std::shared_ptr<Card_data> new_card 
+            = std::make_shared<Card_data>(Card_data{id, top_});
+    top_ = new_card;
+}
+
+void Cards::print(std::ostream& s) {
+   std::shared_ptr<Card_data> to_be_printed = top_;
+   int nr = 1;
+
+   while( to_be_printed != 0 ) {
+      s << nr << ": " << to_be_printed->data << std::endl;
+      to_be_printed = to_be_printed->next;
+      ++nr;
+   }
+}
+
+bool Cards::remove(int &id) {
+    if (top_ == nullptr) {
+        return false;
+    }
+
+    id = top_->data;
+    top_ = top_->next;
+
+    return true;
+}
+
+void Cards::reverse() {
+    std::shared_ptr<Card_data> to_be_inverse = top_;
+    std::shared_ptr<Card_data> prev = nullptr, next = nullptr;
+
+    while (to_be_inverse != nullptr) {
+        next = to_be_inverse->next;
+        to_be_inverse->next = prev;
+        prev = to_be_inverse;
+        to_be_inverse = next;
+    }
+    top_ = prev;
+}
+
+// Do not write the stubs of the methods remove and reverse by yourself here,
+// but open the file reverse.hh and click the declaration of the method
+// by the right mouse button and select
+// Refactor > Add definition in reverse.cpp
